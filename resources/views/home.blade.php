df245a7c6f5b4bafc039d015b529852c9b8e48d0
van khoa <vkhoa6642@gmail.com>
2025-03-14 22:38:58 +0700

    add all

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body style="background: greenyellow">
+
+<h1 style="text-align: center">Trang chủ</h1>
+
+
+    <h1 style="text-align: center">
+        Giờ:
+@php
+echo date('d/m/Y H:i:s');
+@endphp
+</h1>
+
+</body>
+</html>
